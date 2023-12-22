4152eafe59a4de21900fa9ba8bab49e928e2e8bb
Daikenzo Wolfdry Sekaikasou <charles.bru@lapiscine.pro>
2023-12-22 16:31:32 +0100

    achievment of contact form create

--- src/components/public/ContactForm/ContactForm.jsx
+++ src/components/public/ContactForm/ContactForm.jsx
@@ -1,7 +1,8 @@
 import Cookies from "js-cookie";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { API } from "../../../configs/API_config";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 // import { useNavigate } from "react-router-dom";
 
 
@@ -10,7 +11,27 @@ const ContactForm = () => {
     const navigate = useNavigate();
     // const [isEmptyForm, setIsEmptyForm] = useState(true);
     const [isError, setIsError] = useState(false);
+    const [isSuccess, setIsSuccess] = useState(false);
     const [formValidation, setFormValidation] = useState("");
+    const [userInfo, setUserInfo] = useState({
+        username:"",
+        firstname:"",
+        lastname:"",
+        email:"",
+        phone:""
+    });
+    
+
+    // Check and add Info if Login
+    const jwt = Cookies.get("jwt");
+    const user = jwt? jwtDecode(jwt) : "";
+    const fetchUser = async () => {
+        const responseUser = await fetch(`http://${API.defaultpath}/users/${user.data.id}`);
+        const responseUserJs = await responseUser.json();
+        const userInfo = {...responseUserJs.data
+        }
+        setUserInfo(userInfo);
+      };
     
     // Function
     const HandleSendFormSubmit = async (event) => {
@@ -19,10 +40,10 @@ const ContactForm = () => {
         const phoneRegExp = /^((\+)33|0|0033)[1-9](\d{2}){4}$/g;
         // Disabled Reload Page
         event.preventDefault();
-       
+        
 
         // Get Form Value
-        const firstname= event.target.firstname.value;
+        const firstName= event.target.firstName.value;
         const lastName= event.target.lastName.value;
         const email= event.target.email.value;
         const adress = event.target.adress &&{
@@ -31,10 +52,11 @@ const ContactForm = () => {
             postcode : parseInt(event.target.adress.postcode.value),
             city: event.target.adress.city.value
         }
+        
          // const subjectType= event.target.type.value;
         const subjectDescribe= event.target.subject.value;
 
-        if (!firstname || !lastName || !email || !subjectDescribe) {
+        if (!firstName || !lastName || !email || !subjectDescribe) {
             errorMessage += "Veuillez remplir les champs obligatoire"
         }
         const phone= event.target.phone.value;
@@ -48,11 +70,11 @@ const ContactForm = () => {
         if (errorMessage !== "" ){
             setIsError(true)
             return setFormValidation(errorMessage);
-        } else setIsError(false)
+        }
 
         // Set Form Data
         const contactFormData = {
-            senderFirstName:firstname,
+            senderFirstName:firstName,
             senderLastName:lastName,
             senderEmail:email,
             senderPhone:phone !== "" ? phone : null,
@@ -73,43 +95,68 @@ const ContactForm = () => {
     });
 
     const responseCreateJs = await responseCreate.json();
-    
-
+    if (responseCreate.status === 200 || responseCreate.status === 201){
+        errorMessage = "Votre demande a été validé et enregistré" //||  responseCreateJs.message
+        setFormValidation(errorMessage)
+        setIsError(false);
+        setIsSuccess(true);
+        setTimeout(() => {
+            navigate("/");
+          }, "1000");
+        
+    } else{
+        setIsSuccess(false);
+        setIsError(true);
+    }
     };
+
+    // UseState
+    useEffect(()=>{
+        if(Cookies.get("jwt")){
+            fetchUser(jwt);
+        }
+    }, [])
     
     //Display
     return (
         <form onSubmit={HandleSendFormSubmit} className="card ContactForm App-form p-2">
-            {isError && (
+            <>{isError && (
                 <div className="form-item form-control text-bg-danger">
                     <div className="input-group alert-danger d-flex justify-content-center">
                     {formValidation}
                     </div> 
                 </div>)}
+            </>
+            <>{isSuccess && (
+            <div className="form-item form-control text-bg-success">
+                <div className="input-group alert-success d-flex justify-content-center">
+                    {formValidation}
+                    </div> 
+                </div>)}</>
             <div className="form-item input-group">
                 <div className="form-item input-group form-control">
                     <label htmlFor="firstName" className="input-group-text ">Prénom *</label>
-                    <input name="firstname" type="text" defaultValue="Marc" className="input-group-text flex-fill" required/>
+                    <input name="firstName" type="text" defaultValue={userInfo.firstname} className="input-group-text flex-fill" required/>
                 </div>
                 <div className="form-item item-group input-group form-control">
                     <label htmlFor="lastName" className="input-group-text">Nom *</label>
-                    <input name="lastName" type="text" defaultValue="Evans" className="input-group-text flex-fill" required/>
+                    <input name="lastName" type="text" defaultValue={userInfo.lastname} className="input-group-text flex-fill" required/>
                 </div>
             </div>
             <div className="form-item input-group flex-fill">
                 <div className="w-50 form-item item-group input-group form-control">
                     <label htmlFor="email" className="input-group-text">email *</label>
-                    <input name="email" type="email" className="input-group-text flex-fill"/>
+                    <input name="email" type="email" defaultValue={userInfo.email} className="input-group-text flex-fill"/>
                 </div>
                 <div className=" w-25 form-item item-group input-group form-control">
                     <label htmlFor="phone" className="input-group-text">Téléphone</ label>
-                    <input name="phone" type="tel" pattern="[0-9]{10}" className="input-group-text  flex-fill"/>
+                    <input name="phone" type="tel" pattern="[0-9]{10}" defaultValue={userInfo.phone} className="input-group-text  flex-fill"/>
                 </div>
                     
             </div>
                 
             <div className="form-item item-group input-group form-control flex-md-column">
-                <label htmlFor="subject"  className=" input-group-text flex-md-column form-label">Sujet de la demande</label>
+                <label htmlFor="subject"  className=" input-group-text flex-md-column form-label" required>Sujet de la demande</label>
                 <textarea name="subject" id="" cols="30" rows="10"></textarea>
             </div>
             <input className="form-item App-btn btn btn-dark" alt="send" type="submit"/>
